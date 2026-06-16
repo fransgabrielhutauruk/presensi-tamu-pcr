a13d838d2074cb16ba5ee8012067ebb60cef2d74
Frans Gabriel Hutauruk <frans22si@mahasiswa.pcr.ac.id>
2026-06-17 00:34:11 +0700

    update link power bi

--- resources/views/contents/admin/dashboard/eksekutif-dashboard.blade.php
+++ resources/views/contents/admin/dashboard/eksekutif-dashboard.blade.php
@@ -1,17 +1,9 @@
-{{-- <div style="width: 100%; height: min(80vh, 900px);">
-	<iframe
-		title="dashboard-power-bi-presensi-tamu"
-		src="https://app.powerbi.com/view?r=eyJrIjoiNmZjYjEyM2UtNzVkYi00NjE3LThkYTYtNGQ3MGZkZjJjMDA1IiwidCI6ImQ5ZTk3ZDgxLWJiZTUtNGY1Ny05ZDY0LTlkMTYzYTE1NGUyZiIsImMiOjEwfQ%3D%3D&navContentPaneEnabled=false"
-		style="width: 100%; height: 100%; border: 0;"
-		allowfullscreen="true"></iframe>
-</div> --}}
-
-
 <div class="ratio ratio-16x9" data-cy="panel-dashboard-bi">
     <iframe
         data-cy="iframe-dashboard-powerbi"
         title="dashboard-power-bi-presensi-tamu"
-        src="https://app.powerbi.com/view?r=eyJrIjoiZGY3OWE2YzQtNjcyNy00MjQwLWE4YWUtNDhkOWZjNTYwODNjIiwidCI6ImQ5ZTk3ZDgxLWJiZTUtNGY1Ny05ZDY0LTlkMTYzYTE1NGUyZiIsImMiOjEwfQ%3D%3D&pageName=82ac36bc15c82525600a"
+        src="https://app.powerbi.com/view?r=eyJrIjoiM2NkYjQxNTgtOGM2ZC00OTE3LTgxNmEtNTg2MzYxMzNhNGU0IiwidCI6ImQ5ZTk3ZDgxLWJiZTUtNGY1Ny05ZDY0LTlkMTYzYTE1NGUyZiIsImMiOjEwfQ%3D%3D"
+        
         allowfullscreen="true"
         style="border: 0;">
     </iframe>
